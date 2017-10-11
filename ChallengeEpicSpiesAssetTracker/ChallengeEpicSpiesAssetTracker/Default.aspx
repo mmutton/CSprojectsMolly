6f7c9a0e607083b7e5486686f5c0fb055629cc09
Molly <30417297+mmutton@users.noreply.github.com>
2017-10-11 10:38:38 -0700

    Spy Asset Tracker (exercise)
    Three arrays (names, elections rigged, acts of subteguge), add three indexes "{()},{1},{2}}.

---
+++ ChallengeEpicSpiesAssetTracker/ChallengeEpicSpiesAssetTracker/Default.aspx
@@ -0,0 +1,47 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="ChallengeEpicSpiesAssetTracker.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <style type="text/css">
+        .auto-style1 {
+            width: 150px;
+            height: 190px;
+        }
+        .auto-style2 {
+            font-family: Arial, Helvetica, sans-serif;
+        }
+        .auto-style3 {
+            background-color: #A89083;
+        }
+        .auto-style4 {
+            font-family: Arial, Helvetica, sans-serif;
+            color: #453631;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <img alt="" class="auto-style1" src="epic-spies-logo.jpg" /><h2 class="auto-style4">Asset Performance Tracker</h2>
+            <p class="auto-style2">
+                Asset Name&nbsp; <asp:TextBox ID="assetNameTextBox" runat="server" Width="144px"></asp:TextBox>
+            </p>
+            <p class="auto-style2">
+                Number of
+                Elections Rigged&nbsp; <asp:TextBox ID="electionsRiggedTextBox" runat="server" Height="19px" Width="46px"></asp:TextBox>
+            </p>
+            <p class="auto-style2">
+                Number of
+                Acts of Subterfuge&nbsp; <asp:TextBox ID="actsOfSubterfugeTextBox" runat="server" Height="19px" Width="46px"></asp:TextBox>
+            </p>
+            <p class="auto-style2" style="color: #00FFFF">
+                <asp:Button ID="addButton" runat="server" CssClass="auto-style3" OnClick="addButton_Click" Text="Add Asset" BorderColor="#453631" BorderStyle="Solid" Height="48px" Width="80px" />
+            </p>
+        </div>
+        <asp:Label ID="resultLabel" runat="server"></asp:Label>
+    </form>
+</body>
+</html>
---
+++ ChallengeEpicSpiesAssetTracker/ChallengeEpicSpiesAssetTracker/Default.aspx.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ChallengeEpicSpiesAssetTracker
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!Page.IsPostBack)
+            {
+                string[] assets = new string[0];
+                int[] elections = new int[0];
+                int[] acts = new int[0];
+
+                ViewState.Add("Assets", assets);
+                ViewState.Add("Elections", elections);
+                ViewState.Add("Acts", acts);
+            }
+        }
+
+        protected void addButton_Click(object sender, EventArgs e)
+        {
+            string[] assets = (String[])ViewState["Assets"];
+            int[] elections = (int[])ViewState["Elections"];
+            int[] acts = (int[])ViewState["Acts"];
+
+            int newLength = assets.Length + 1;
+
+            Array.Resize(ref assets, newLength);
+            Array.Resize(ref elections, newLength);
+            Array.Resize(ref acts, newLength);
+
+            int newIndex = assets.GetUpperBound(0);
+
+            assets[newIndex] = assetNameTextBox.Text;
+            elections[newIndex] = int.Parse(electionsRiggedTextBox.Text);
+            acts[newIndex] = int.Parse(actsOfSubterfugeTextBox.Text);
+
+            ViewState["Assets"] = assets;
+            ViewState["Elections"] = elections;
+            ViewState["Acts"] = acts;
+
+            resultLabel.Text = String.Format("Total Elections Rigged: {0}" +
+                "<br>Average acts of subterfuge per Asset:{1:N2}" +
+                "<br>(Last Asset you added: {2})",
+                elections.Sum(),
+                acts.Average(),
+                assets[newIndex]);
+
+            assetNameTextBox.Text = "";
+            electionsRiggedTextBox.Text = "";
+            actsOfSubterfugeTextBox.Text = "";
+
+        }
+    }
+}
