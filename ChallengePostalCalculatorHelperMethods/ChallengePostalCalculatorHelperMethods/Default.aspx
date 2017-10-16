722c75a40a3de0eac868a5421c98a9901fcedab8
Molly <30417297+mmutton@users.noreply.github.com>
2017-10-16 16:35:20 -0700

    Postal Calculator Drill
    Drill to create a postage calculator using at least two methods.  Calculates volume of package times three different shipping rates.

---
+++ ChallengePostalCalculatorHelperMethods/ChallengePostalCalculatorHelperMethods/Default.aspx
@@ -0,0 +1,40 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="ChallengePostalCalculatorHelperMethods.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <style type="text/css">
+        .auto-style1 {
+            font-family: Arial, Helvetica, sans-serif;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <span class="auto-style1">Postal Calculator</span><br />
+            <br />
+            Width:&nbsp;&nbsp;
+            <asp:TextBox ID="widthTextBox" runat="server" AutoPostBack="True" Height="22px" OnTextChanged="handleChange" Width="69px"></asp:TextBox>
+            <br />
+            <br />
+            Height:&nbsp;&nbsp; <asp:TextBox ID="heightTextBox" runat="server" AutoPostBack="True" Height="22px" OnTextChanged="handleChange" Width="69px"></asp:TextBox>
+            <br />
+            <br />
+            Length:&nbsp;
+            <asp:TextBox ID="lengthTextBox" runat="server" AutoPostBack="True" Height="22px" OnTextChanged="handleChange" Width="69px"></asp:TextBox>
+            <br />
+        </div>
+        <asp:RadioButton ID="groundRadioButton" runat="server" GroupName="ShippingGroup" Text="Ground" AutoPostBack="True" OnCheckedChanged="handleChange" />
+        <br />
+        <asp:RadioButton ID="airRadioButton" runat="server" GroupName="ShippingGroup" Text="Air" AutoPostBack="True" OnCheckedChanged="handleChange" />
+        <br />
+        <asp:RadioButton ID="nextDayRadioButton" runat="server" GroupName="ShippingGroup" Text="Next Day" AutoPostBack="True" OnCheckedChanged="handleChange" />
+        <br />
+        <br />
+        <asp:Label ID="resultLabel" runat="server"></asp:Label>
+    </form>
+</body>
+</html>
---
+++ ChallengePostalCalculatorHelperMethods/ChallengePostalCalculatorHelperMethods/Default.aspx.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ChallengePostalCalculatorHelperMethods
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+           
+        }
+        protected void handleChange(object sender, EventArgs e)
+
+        {
+    
+            if (!valuesExist()) return;
+
+            //What is the volume
+            int volume = 0;
+            if (!tryGetVolume(out volume)) return;
+
+            //What is the shipping multiplier(.15, .25, .45) times the volume.
+            double postageMultiplier = getPostageMultiplier();
+
+            //Determine total cost
+            double cost = volume * postageMultiplier;
+
+            //Display results to user.
+            resultLabel.Text = String.Format("Your parcel will cost " +
+                " {0:C} to ship.", cost);
+        }
+
+        private bool valuesExist()
+        {
+            if (!airRadioButton.Checked
+                 && !groundRadioButton.Checked
+                 && !nextDayRadioButton.Checked)
+                return false;
+
+            if (widthTextBox.Text.Trim().Length == 0
+                || heightTextBox.Text.Trim().Length == 0)
+                return false;
+            else
+                return true;
+        }
+        private bool tryGetVolume(out int volume)
+        {
+            volume = 0;
+            int width = 0;
+            int height = 0;
+            int length = 0;
+
+            if (!int.TryParse(widthTextBox.Text.Trim()
+                , out width)) return false;
+            if (!int.TryParse(heightTextBox.Text.Trim()
+                , out height)) return false;
+            if (!int.TryParse(lengthTextBox.Text.Trim()
+                , out length)) length = 1;
+
+            volume = width * height * length;
+            
+            return true;
+        }
+        private double getPostageMultiplier()
+        {
+            if (groundRadioButton.Checked) return .15;
+            else if (airRadioButton.Checked) return .25;
+            else if (nextDayRadioButton.Checked) return .45;
+            else return 0;
+
+        }
+    }
+}
