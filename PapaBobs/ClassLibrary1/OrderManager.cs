a24df2dccdf9b23c2842fc6cd6cd6ea1e182b723
Molly <30417297+mmutton@users.noreply.github.com>
2017-11-14 11:20:18 -0800

    Mega Pizza Order Challenge
    Papa Bob's Pizza challenge encompasses 4 projects controlling the domain, DTO, persistence and presentation layers and including a database with 2 tables for managing the orders and prices.   Created several classes and incorporated exception handling and user notices.

---
+++ PapaBobs/ClassLibrary1/OrderManager.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PapaBobs.Domain
+
+{
+    public class OrderManager
+    {
+        public static void CreateOrder(DTO.OrderDTO orderDTO)
+        {
+            //validation 
+            if (orderDTO.Name.Trim().Length == 0)
+                throw new Exception("Name required.");
+
+            if (orderDTO.Address.Trim().Length == 0)
+                throw new Exception("Address required.");
+
+            if (orderDTO.Zip.Trim().Length == 0)
+                throw new Exception("Zip code required.");
+
+            if (orderDTO.Phone.Trim().Length == 0)
+                throw new Exception("Phone number required.");
+
+            orderDTO.OrderId = Guid.NewGuid();
+            orderDTO.TotalCost = PizzaPriceManager.CalculateCost(orderDTO);
+            Persistence.OrderRepository.CreateOrder(orderDTO);
+        }
+
+        public static object GetOrders()
+        {
+            return Persistence.OrderRepository.GetOrders();
+        }
+
+        public static void CompleteOrder(Guid orderId)
+        {
+            Persistence.OrderRepository.CompleteOrder(orderId);
+        }
+    }
+}
